5c771e96f7f07b08c0802334827f8d4fa2d59b66
Torsten Brischalle <torsten@brischalle.de>
2026-02-22 01:28:08 +0100

    All calculator buttons created

---
+++ app/src/main/java/com/example/ti36calculator/ColorDefinitions.kt
@@ -0,0 +1,22 @@
+package com.example.ti36calculator
+
+import androidx.compose.ui.graphics.Color
+
+val CASE_COLOR = Color(0xFF4B565A)
+
+//val BUTTON_COLOR_1 = Color(0xFF142924)
+//val BUTTON_COLOR_2 = Color(0xFF545D5C)
+//val BUTTON_COLOR_3 = Color(0xFFAAAFAB)
+
+val BUTTON_COLOR_1 = Color(0xFF213228)
+val BUTTON_COLOR_2 = Color(0xFF6E86C6)
+val BUTTON_COLOR_3 = Color(0xFF8E8E8E)
+
+val TEXT_1ST_COLOR_1 = Color(0xFFE3EDEE)
+val TEXT_1ST_COLOR_2 = Color(0xFF2D3834)
+
+val TEXT_2ND_COLOR = Color(0xFFF9BF6B)
+
+val TEXT_3RD_COLOR = Color(0xFFC7D1D3)
+
+val TEXT_4TH_COLOR = Color(0xFF55ADD5)
--- app/src/main/java/com/example/ti36calculator/MainActivity.kt
+++ app/src/main/java/com/example/ti36calculator/MainActivity.kt
@@ -4,29 +4,44 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ti36calculator.ui.theme.Ti36CalculatorTheme
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 
 class MainActivity : ComponentActivity() {
+
+    /**
+     * The main activity of the TI-36 calculator app.
+     * It sets up the UI using Jetpack Compose and displays the calculator buttons in a grid layout.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Ti36CalculatorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(modifier = Modifier.padding(innerPadding)) {
-                        CalculatorButtons()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = CASE_COLOR
+                ) {
+                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                        Box(
+                            modifier = Modifier
+                                .padding(innerPadding)
+                                .background(CASE_COLOR)
+                        ) {
+                            CalculatorButtons()
+                        }
                     }
                 }
             }
@@ -34,13 +49,21 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+/**
+ * Displays a single calculator button with its properties.
+ * The button has two lines of text: text1st and text2nd.
+ * Above the button, there are two additional lines of text: text3rd and text4th.
+ * If text4th is empty, text3rd is centered. Otherwise, text3rd is left-aligned and text4th is right-aligned.
+ * @param buttonProperties The properties of the calculator button, including text and colors.
+ * @param modifier The modifier to be applied to the button layout.
+ */
 @Composable
-fun CalculatorButton(text1: String, text2: String, text3: String, text4: String, modifier: Modifier) {
+fun CalculatorButton(buttonProperties : CalculatorButtonProperties, modifier: Modifier) {
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        if (text4.isEmpty()) {
+        if (buttonProperties.text4th.isEmpty()) {
             // center text3 if text4 is empty
             Row(
                 modifier = Modifier.fillMaxWidth(),
@@ -48,7 +71,9 @@ fun CalculatorButton(text1: String, text2: String, text3: String, text4: String,
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
-                    text = text3,
+                    text = buttonProperties.text3rd,
+                    fontSize = 16.sp,
+                    color = buttonProperties.test3rdColor,
                     maxLines = 1
                 )
             }
@@ -59,14 +84,18 @@ fun CalculatorButton(text1: String, text2: String, text3: String, text4: String,
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
-                    text = text3,
-                    modifier = Modifier.weight(1f),
+                    text = buttonProperties.text3rd,
+                    fontSize = 16.sp,
+                    color = buttonProperties.test3rdColor,
+                    modifier = Modifier.weight(1f).padding(start = 5.dp),
                     maxLines = 1
                 )
                 Spacer(modifier = Modifier.width(3.dp))
                 Text(
-                    text = text4,
-                    modifier = Modifier.weight(1f),
+                    text = buttonProperties.text4th,
+                    fontSize = 16.sp,
+                    color = buttonProperties.test4thColor,
+                    modifier = Modifier.weight(1f).padding(end = 5.dp),
                     maxLines = 1,
                     textAlign = TextAlign.End
                 )
@@ -75,41 +104,53 @@ fun CalculatorButton(text1: String, text2: String, text3: String, text4: String,
         Button(
             onClick = {},
             modifier = Modifier.fillMaxWidth(),
-            shape = androidx.compose.foundation.shape.RoundedCornerShape(4.dp),
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF505050)),
+            shape = RoundedCornerShape(5.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = buttonProperties.buttonColor),
             contentPadding = PaddingValues(2.dp)
         ) {
             Column(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = text1)
+                Text(
+                    text = buttonProperties.text2nd,
+                    fontSize = 16.sp,
+                    color = buttonProperties.test2ndColor,
+                    maxLines = 1
+                )
                 HorizontalDivider(modifier = Modifier.fillMaxWidth())
-                Text(text = text2)
+                Text(
+                    text = buttonProperties.text1st,
+                    fontSize = 18.sp,
+                    color = buttonProperties.test1stColor,
+                    maxLines = 1
+                )
             }
         }
     }
 }
 
+/**
+ * Displays the calculator buttons in a grid layout.
+ * The grid has 5 columns and fills the available space.
+ * If a button's text1st is empty, the grid cell is empty.
+ */
 @Composable
 fun CalculatorButtons() {
-    Column(modifier = Modifier.fillMaxSize()) {
-        var buttonNumber = 1
-        for (row in 1..8) {
-            Row(modifier = Modifier.weight(1f)) {
-                Spacer(modifier = Modifier.width(4.dp))
-                for (col in 1..5) {
-                    CalculatorButton(
-                        "$buttonNumber", "2nd", "3rd",
-                        if (col == 3) "" else "4th",
-                        Modifier.weight(1f)
-                    )
-                    Spacer(modifier = Modifier.width(4.dp))
-                    buttonNumber++
-                }
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(5),
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(4.dp)
+    ) {
+        items(CALCULATOR_BUTTON_LIST) { buttonProperties ->
+            if (buttonProperties.text1st.isEmpty()) {
+                Spacer(modifier = Modifier.padding(0.dp))
             }
-            if (row < 8) {
-                Spacer(modifier = Modifier.height(4.dp))
+            else {
+                CalculatorButton(
+                    buttonProperties = buttonProperties,
+                    Modifier.padding(4.dp).fillMaxWidth()
+                )
             }
         }
     }
