2bc26c538928c346f197c115a77d953b1dc33ed7
Torsten Brischalle <torsten@brischalle.de>
2026-03-08 23:37:24 +0100

    Calculator view model completed

--- app/src/main/java/com/bri1234/ti36calculator/Ti36Simulator.kt
+++ app/src/main/java/com/bri1234/ti36calculator/Ti36Simulator.kt
@@ -1,11 +1,61 @@
 package com.bri1234.ti36calculator
 
+import kotlin.CharArray
+import kotlin.charArrayOf
+
 /**
  * A simulator class for the TI-36 calculator.
  */
 class Ti36Simulator {
 
-    fun buttonPressed(button: CalculatorButton) {
-        println("Button pressed: $button")
+    fun getDisplayState(): CalculatorDisplayState {
+        // Return a dummy display state for now
+        return CalculatorDisplayState(
+            digitsLarge = charArrayOf(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '0'),
+            decimalPointIndex = 10,
+            digitsSmall = charArrayOf(' ', ' ', ' '),
+            displayLabels = mutableSetOf(DisplayLabels.DEG),
+        )
     }
+
+    fun buttonPressedAcOn() {}
+    fun buttonPressedThird() {}
+    fun buttonPressedHyp() {}
+    fun buttonPressedLog() {}
+    fun buttonPressedLn() {}
+    fun buttonPressedCeC() {}
+    fun buttonPressedSecond() {}
+    fun buttonPressedSin() {}
+    fun buttonPressedCos() {}
+    fun buttonPressedTan() {}
+    fun buttonPressedYPow() {}
+    fun buttonPressedXSwapY() {}
+    fun buttonPressedOneDivX() {}
+    fun buttonPressedXSquared() {}
+    fun buttonPressedSqrtX() {}
+    fun buttonPressedDivide() {}
+    fun buttonPressedSumPlus() {}
+    fun buttonPressedEE() {}
+    fun buttonPressedLeftParentheses() {}
+    fun buttonPressedRightParentheses() {}
+    fun buttonPressedMultiply() {}
+    fun buttonPressedStore() {}
+    fun buttonPressedSeven() {}
+    fun buttonPressedEight() {}
+    fun buttonPressedNine() {}
+    fun buttonPressedMinus() {}
+    fun buttonPressedRecall() {}
+    fun buttonPressedFour() {}
+    fun buttonPressedFive() {}
+    fun buttonPressedSix() {}
+    fun buttonPressedPlus() {}
+    fun buttonPressedABC() {}
+    fun buttonPressedOne() {}
+    fun buttonPressedTwo() {}
+    fun buttonPressedThree() {}
+    fun buttonPressedEqual() {}
+    fun buttonPressedBack() {}
+    fun buttonPressedZero() {}
+    fun buttonPressedDot() {}
+    fun buttonPressedPlusMinus() {}
 }
